d4566a5fa2e6ce1fa29843a96424f08f52f71ddf
Peterson Bersanetti <74914733+petersonbersanetti@users.noreply.github.com>
2023-06-22 18:16:44 -0300

    Criando paginas Html Formulario e Lista

--- src/main/java/br/com/alura/screematch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screematch/controller/FilmeController.java
@@ -3,6 +3,7 @@ package br.com.alura.screematch.controller;
 import br.com.alura.screematch.domain.filme.DadosCadastroFilme;
 import br.com.alura.screematch.domain.filme.Filme;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +16,22 @@ import java.util.List;
 public class FilmeController {
 
     private List<Filme> filmes = new ArrayList<>();
-    @GetMapping
+    @GetMapping("/formulario")
     public String carregaPaginaFormulario (){
         return "filmes/formulario";
+    }
+
+    @GetMapping
+    public String carregaPaginaListagem (Model model){
+        model.addAttribute("lista", filmes);
+        return "filmes/listagem";
     }
 
     @PostMapping
     public String cadastraFilmes(DadosCadastroFilme dados){
         var filme = new Filme(dados);
         filmes.add(filme);
-        return "filmes/formulario";
+        return "redirect:/filmes";
     }
 
 }
